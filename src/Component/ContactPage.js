2c902c385181b7e0c5329e6f91920ed7dd7373d8
khititach <khititach58@email.nu.ac.th>
2020-11-18 22:26:10 +0700

    add Download cv

--- src/Component/ContactPage.js
+++ src/Component/ContactPage.js
@@ -3,9 +3,17 @@ import React from 'react'
 import Footer from './footer'
 
 import {DataEn} from './DataEn'
+import CV from '../Image/AF-cv01.jpg'
 
 function ContactPage () {
-
+    function DownloadCV() {
+        var link = document.createElement('a');
+        link.href = CV;
+        link.download = 'cv.jpg';
+        document.body.appendChild(link);
+        link.click();
+     
+    }
     return (
         <>
             <div id="contact" className="App-contact-header">
@@ -27,7 +35,9 @@ function ContactPage () {
                             <span className="contact-span">: <a href="https://www.instagram.com/shirokitsunee/">{DataEn.Contact.Instagram.name}</a> </span>
                         </div>
                     </div>
-                   
+                   <div className="download-cv">
+                       <button className="btn-download" onClick={DownloadCV}>Download CV</button>
+                   </div>
                 </div>
               
             </div>
